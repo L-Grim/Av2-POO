101e46290903a620045588e0c0bc74a1444243e0
L-Grim <111025216+L-Grim@users.noreply.github.com>
2023-05-10 00:32:23 -0300

    Add files via upload

---
+++ Av-2 POO/src/agenda/Agenda.java
@@ -0,0 +1,25 @@
+package agenda;
+
+import contatos.Contato;
+
+public class Agenda {
+	
+	Contato contatos[] = new Contato[20];
+	
+	public void addContato(Contato contato, int index) {
+		contatos[index] = contato;
+	}
+	
+	public void listarContatos() {
+		for (int index = 0; index < contatos.length; index++) {
+			if (contatos[0] == null) {
+				System.out.println("A agenda está vazia.");
+			} else if(contatos[index] == null) {}
+			else {
+				System.out.println("------------------------------");
+					System.out.println(contatos[index]);
+					System.out.println("------------------------------");
+			}
+		}
+	}
+}
